b0b6ba8d662991db3274d52fa8e0efa61d6f6547
nomi-nonsz <me@nonszy.space>
2026-09-06 01:34:38 +0700

    Add install script

--- install.sh
+++ install.sh
@@ -3,12 +3,53 @@
 set -e
 
 INSTALL_DIR=$HOME/.local/bin
+APP_NAME=video-caption-translator
+APP_VERSION=v1.1.0
+BASE_URL=https://github.com/nomi-nonsz/video-caption-translator/releases/download
+
+OS="idk"
+ARCH="idk"
+
+echo "Started $APP_NAME install script."
 
 if [ ! -d "$INSTALL_DIR" ]; then
   mkdir -p "$INSTALL_DIR"
 fi
 
-curl -sSL https://example.com/app -o "$INSTALL_DIR/app"
-chmod +x "$INSTALL_DIR/app"
+OS_CHECK="$(uname -s)"
+case "$(uname -s)" in
+  Linux*) OS="linux";;
+  Darwin*) OS="macos";;
+  *)
+    echo "Unsupported OS: $OS_CHECK"
+    echo "Sorry :("
+    exit 1
+    ;;
+esac
 
-echo "Completed."
+ARCH_CHECK="$(uname -m)"
+case "$ARCH_CHECK" in
+  x86_64 | amd64) ARCH="amd64";;
+  arm64) ARCH="arm64";;
+  *)
+    echo "Unsupported architecture: $ARCH_CHECK"
+    echo "Sorry :("
+    exit 1
+    ;;
+esac
+
+FULL_URL="$BASE_URL/$APP_VERSION/$APP_NAME-$APP_VERSION-$OS-$ARCH"
+
+echo "Downloading $APP_NAME-$APP_VERSION-$OS-$ARCH..."
+
+curl -sSL "$FULL_URL" -o "$INSTALL_DIR/$APP_NAME"
+chmod +x "$INSTALL_DIR/$APP_NAME"
+
+echo "Completed ✔"
+
+if [[ -f "$INSTALL_DIR/$APP_NAME" ]]; then
+  echo "Stored at $INSTALL_DIR/$APP_NAME"
+  echo "If the terminal can't find the app, try to add this in your shell configuration (.bashrc, .zshrc, etc.):"
+  echo ""
+  echo "  export PATH="\$HOME/.local/bin:\$PATH""
+fi
